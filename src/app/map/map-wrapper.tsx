337dd22a1ee49c831f82429e5369689546fb70c3
FirstFlush <firstflush@protonmail.com>
2025-02-15 08:29:29 -0800

    added spinning loader to map

--- src/app/map/map-wrapper.tsx
+++ src/app/map/map-wrapper.tsx
@@ -3,9 +3,9 @@ import dynamic from "next/dynamic"
 import { MapData } from "@/types/MapData"
 import { useState, useEffect } from "react"
 import { fetchResources } from "@/api/resources"
-// import Map from "@/components/map"
-const Map = dynamic(() => import("@/app/map/map"), { ssr: false })
+import { Loader, AlertCircle } from "lucide-react"
 
+const Map = dynamic(() => import("@/app/map/map"), { ssr: false })
 
 const MapWrapper = () => {
   const [mapData, setMapData] = useState<MapData | null>(null)
@@ -19,7 +19,7 @@ const MapWrapper = () => {
         console.log(data)
         setMapData(data)
       } catch (err) {
-        setError("Failed to load resources")
+        setError("Failed to load map data. Please try again later.")
       } finally {
         setLoading(false)
       }
@@ -27,19 +27,33 @@ const MapWrapper = () => {
     loadResources()
   }, [])
 
-  if (loading) return <p>Loading map...</p>
-  if (error) return <p>{error}</p>
-  if (!mapData) return <p>No map data available</p> 
+  if (loading) {
+    return (
+      <div className="flex flex-col items-center justify-center h-64 text-gray-600 pt-24">
+        <Loader className="animate-spin" size={32} />
+        <p className="mt-2 text-lg">Loading map...</p>
+      </div>
+    )
+  }
 
-  return (
-    <div>
-      <Map mapData={mapData} />
-    </div>
-  ) 
+  if (error) {
+    return (
+      <div className="flex flex-col items-center justify-center h-64 bg-red-100 text-red-600 p-8 pt-24 rounded-lg ">
+        <AlertCircle size={32} />
+        <p className="mt-2 text-lg">{error}</p>
+      </div>
+    )
+  }
 
-//   return <div>blehhh</div>
+  if (!mapData) {
+    return (
+      <div className="flex flex-col items-center justify-center h-64 text-gray-500">
+        <p className="text-lg">No map data available.</p>
+      </div>
+    )
+  }
 
+  return <Map mapData={mapData} />
 }
 
-
-export default MapWrapper;
+export default MapWrapper
