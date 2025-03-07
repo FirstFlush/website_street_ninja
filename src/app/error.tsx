806943e52cea0998db75ad645850db347a9532a5
FirstFlush <firstflush@protonmail.com>
2025-03-07 08:12:11 -0800

    Added 404 and 500 error pages

---
+++ src/app/error.tsx
@@ -0,0 +1,36 @@
+import { Button } from "@/components/ui/Button"
+import { ArrowRightIcon } from 'lucide-react';
+import SectionHeader from "@/components/layout/section-header";
+
+
+export default function Error() {
+  return (
+      <div className="grid min-h-full sm:place-items-center px-6 py-44 sm:py-32 lg:px-8">
+        <div className="text-center">
+          <p className="text-xl font-semibold text-primary-dark-10 glitch">500</p>
+          <SectionHeader 
+            title="An unexpected error occurred" 
+            subtitle="Sorry! We have been notified of this error and will work to fix it ASAP."
+          />
+          <div className="mt-10 flex items-center justify-center gap-x-6">
+            <Button
+              href="/"
+              color="primary"
+            >
+              Go back home
+            </Button>
+            <Button
+              href="/try-it-out"
+              variant="outline"
+              className="group"
+            >
+              <span className="mr-1">Try it out</span>
+              <ArrowRightIcon className="w-4 h-4 group-hover:text-primary-dark-10 transition-colors duration-300"/>
+            </Button>
+          </div>
+        </div>
+    </div>
+  )
+}
+
+
--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,27 +1,64 @@
-import Link from 'next/link'
+import { Button } from "@/components/ui/Button"
+import { ArrowRightIcon } from 'lucide-react';
+import SectionHeader from "@/components/layout/section-header";
 
-import { Button } from '@/components/ui/Button'
-import Logo from '@/components/logo'
-import { SlimLayout } from '@/components/layout/slim-layout'
 
 export default function NotFound() {
   return (
-    <SlimLayout>
-      <div className="flex">
-        <Link href="/" aria-label="Home">
-          <Logo className="h-10 w-auto" />
-        </Link>
-      </div>
-      <p className="mt-20 text-sm font-medium text-gray-700">404</p>
-      <h1 className="mt-3 text-lg font-semibold text-gray-900">
-        Page not found
-      </h1>
-      <p className="mt-3 text-sm text-gray-700">
-        Sorry, we couldn’t find the page you’re looking for.
-      </p>
-      <Button href="/" className="mt-10">
-        Go back home
-      </Button>
-    </SlimLayout>
+      <div className="grid min-h-full sm:place-items-center px-6 py-44 sm:py-32 lg:px-8">
+        <div className="text-center">
+          <p className="text-xl font-semibold text-primary-dark-10 glitch">404</p>
+          <SectionHeader 
+            title="Page not found" 
+            subtitle="Sorry, we couldn’t find the page you’re looking for."
+          />
+          <div className="mt-10 flex items-center justify-center gap-x-6">
+            <Button
+              href="/"
+              color="primary"
+            >
+              Go back home
+            </Button>
+            <Button
+              href="/try-it-out"
+              variant="outline"
+              className="group"
+            >
+              <span className="mr-1">Try it out</span>
+              <ArrowRightIcon className="w-4 h-4 group-hover:text-primary-dark-10 transition-colors duration-300"/>
+            </Button>
+          </div>
+        </div>
+    </div>
   )
 }
+
+
+
+// import Link from 'next/link'
+
+// import { Button } from '@/components/ui/Button'
+// import Logo from '@/components/logo'
+// import { SlimLayout } from '@/components/layout/slim-layout'
+
+// export default function NotFound() {
+//   return (
+//     <SlimLayout>
+//       <div className="flex">
+//         <Link href="/" aria-label="Home">
+//           <Logo className="h-10 w-auto" />
+//         </Link>
+//       </div>
+//       <p className="mt-20 text-sm font-medium text-gray-700">404</p>
+//       <h1 className="mt-3 text-lg font-semibold text-gray-900">
+//         Page not found
+//       </h1>
+//       <p className="mt-3 text-sm text-gray-700">
+//         Sorry, we couldn’t find the page you’re looking for.
+//       </p>
+//       <Button href="/" className="mt-10">
+//         Go back home
+//       </Button>
+//     </SlimLayout>
+//   )
+// }
