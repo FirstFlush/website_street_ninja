ee7d9cf8de7e02e1f6d21c788b990d6afd278fef
FirstFlush <firstflush@protonmail.com>
2025-06-27 15:39:24 -0700

    feat: added loading spinner to popover-content - also removed unnecessary fragment from reasons-desktop.tsx

--- src/app/map/popover-content.tsx
+++ src/app/map/popover-content.tsx
@@ -3,27 +3,38 @@ import React from 'react'
 import { useEffect, useState } from 'react'
 import { keywords } from '@/data/keywords'
 import { fetchMapPinSingle } from '@/api/resources'
+import { Loader } from 'lucide-react'
 
 interface PopoverContentProps {
   id: number
   resourceType: string
 }
 
+interface PopoverState {
+  data: string | null;
+  status: "loading" | "success" | "error" | "idle";
+}
+
 const PopoverContent = ({ id, resourceType }: PopoverContentProps) => {
 
-  const [popoverData, setPopoverData] = useState<string | null>(null)
+  const [popoverData, setPopoverData] = useState<PopoverState>({data: null, status: "idle" })
   
   useEffect(() => {
     async function loadMapData() {
+      setPopoverData(prev => ({...prev, status: "loading"}))
       try {
         const res = await fetchMapPinSingle(resourceType, id);
-        
         if (res.data){
-          setPopoverData(res.data)
+          setPopoverData({
+            status: "success",
+            data: res.data,
+          })
         }
       }
       catch (err) {
-        if (process.env.NODE_ENV == "development") console.log(err);
+        if (process.env.NODE_ENV == "development") 
+          console.log(err);
+        setPopoverData(prev => ({...prev, status: "error"}))
       }
     }
     loadMapData()
@@ -47,14 +58,12 @@ const PopoverContent = ({ id, resourceType }: PopoverContentProps) => {
       });
   };
 
-
   const keywordData = keywords.find(
     (keyword) => keyword.title.toLowerCase() === resourceType.toLowerCase()
   )
 
-  if (!popoverData || popoverData.length === 0)
-    return <p className="text-sm text-gray-500">No details available</p>
-
+  if (popoverData.status == "error") return <p className="text-sm text-gray-500">No details available</p>
+  else if (popoverData.status == "loading") return <p className="text-sm text-gray-500 w-[100%]"><Loader className="mx-auto animate-spin" size={22} /></p>
   return (
     <div className="text-xs">
 
@@ -68,77 +77,10 @@ const PopoverContent = ({ id, resourceType }: PopoverContentProps) => {
         <span className="ml-1">{resourceType}</span>
       </h6>
       <div className="mt-[-1.25rem]">
-        {renderFormattedText(popoverData)}
+        {renderFormattedText(popoverData.data ?? "No details available")}
       </div>
-      {/* <p>
-        {
-          popoverData.split("\n\n").map((line, i) => (
-            <p key={i}>
-              {line}
-            </p>
-          ))
-        }
-      </p> */}
     </div>
   )
 }
 
 export default PopoverContent
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// 'use client'
-// import React from 'react'
-// import clsx from 'clsx'
-// import { keywords } from '@/data/keywords'
-
-// interface PopoverContentProps {
-//   data: Record<string, any>
-//   resourceType: string
-// }
-
-// const PopoverContent = ({ data, resourceType }: PopoverContentProps) => {
-
-//   const keywordData = keywords.find(
-//     (keyword) => keyword.title.toLowerCase() === resourceType.toLowerCase()
-//   )
-
-//   if (!data || Object.keys(data).length === 0)
-//     return <p className="text-sm text-gray-500">No details available</p>
-
-//   return (
-//     <div className="text-xs">
-//       <h6 className="mt-4 mb-1 font-semibold tracking-wide text-white bg-primary shadow-md px-2 py-1 rounded-lg max-w-fit capitalize">
-//         { keywordData ? <keywordData.icon className="inline-block align-text-bottom" size={18} /> : "" } 
-//         <span className="ml-1">{resourceType}</span>
-//       </h6>
-//       <table className="w-full">
-//           <tbody>
-//           {Object.entries(data).map(([key, value], index) => (
-//               <tr key={key} className={clsx(
-//                   "border-gray-200",
-//                   index !== 0 && "border-t",
-//               )}>
-//               <td className="py-1 font-semibold capitalize text-gray-700">{key.replace(/_/g, " ")}</td>
-//               <td className="pl-2 py-1 text-gray-600">{String(value)}</td>
-//               </tr>
-//           ))}
-//           </tbody>
-//       </table>
-//     </div>
-//   )
-// }
-
-// export default PopoverContent
--- src/components/sections/why-it-matters/reasons-desktop.tsx
+++ src/components/sections/why-it-matters/reasons-desktop.tsx
@@ -6,7 +6,6 @@ function ReasonsDesktop() {
   return (
     <TabGroup className="hidden lg:mt-20 lg:block">
       {({ selectedIndex }) => (
-        <>
           <TabList className="grid grid-cols-3 gap-x-8">
             {reasons.map((reason, reasonIndex) => (
               <ReasonDiv
@@ -26,33 +25,6 @@ function ReasonsDesktop() {
               />
             ))}
           </TabList>
-          {/* <TabPanels className="relative mt-20 overflow-hidden rounded-4xl bg-slate-200 px-14 py-16 xl:px-16">
-            <div className="-mx-5 flex">
-              {reasons.map((reason, reasonIndex) => (
-                <TabPanel
-                  static
-                  key={reason.summary}
-                  className={clsx(
-                    'px-5 transition duration-500 ease-in-out data-selected:not-data-focus:outline-hidden',
-                    reasonIndex !== selectedIndex && 'opacity-60',
-                  )}
-                  style={{ transform: `translateX(-${selectedIndex * 100}%)` }}
-                  aria-hidden={reasonIndex !== selectedIndex}
-                >
-                  <div className="w-[52.75rem] overflow-hidden rounded-xl bg-white ring-1 shadow-lg shadow-slate-900/5 ring-slate-500/10">
-                    <Image
-                      className="w-full"
-                      src={reason.image}
-                      alt=""
-                      sizes="52.75rem"
-                    />
-                  </div>
-                </TabPanel>
-              ))}
-            </div>
-            <div className="pointer-events-none absolute inset-0 rounded-4xl ring-1 ring-slate-900/10 ring-inset" />
-          </TabPanels> */}
-        </>
       )}
     </TabGroup>
   )
