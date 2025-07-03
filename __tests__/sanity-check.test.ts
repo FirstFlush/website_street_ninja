664e5ba72a6e123175578ca8bc9f7c46d45fd0a0
FirstFlush <firstflush@protonmail.com>
2025-07-02 17:44:41 -0700

    test: Configured Jest testing environment - installed all required packages - configured jest.config.js and jest.setup.js files - added test running to package scripts - wrote and successfully executed sanity-check test to ensure Jest testing environment was properly configured

---
+++ __tests__/sanity-check.test.ts
@@ -0,0 +1,5 @@
+describe('Sanity test', () => {
+  it('runs a basic truthy check to ensure Jest configuration', () => {
+    expect(true).toBe(true);
+  });
+});
---
+++ jest.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+    '\\.(css|scss|sass)$': 'identity-obj-proxy',
+  },
+  testMatch: ['**/?(*.)+(test|spec).[jt]s?(x)'],
+};
